26e9456e634c604781a092487c93f7bac171dda7
Joe Fox <joefoxdevelopment@gmail.com>
2018-03-01 14:39:43 +0000

    Add reset password command

--- app/Console/Commands/ResetPassword.php
+++ app/Console/Commands/ResetPassword.php
@@ -2,16 +2,21 @@
 
 namespace App\Console\Commands;
 
+use App\User;
+use Cerbero\CommandValidator\ValidatesInput;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Validator;
 
 class ResetPassword extends Command
 {
+    use ValidatesInput;
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'user:reset-password';
+    protected $signature = 'user:reset-password {email}';
 
     /**
      * The console command description.
@@ -30,6 +35,13 @@ class ResetPassword extends Command
         parent::__construct();
     }
 
+    public function rules()
+    {
+        return [
+            'email' => 'required|string|email|max:255',
+        ];
+    }
+
     /**
      * Execute the console command.
      *
@@ -37,6 +49,46 @@ class ResetPassword extends Command
      */
     public function handle()
     {
-        //
+        $email = $this->argument('email');
+        $user  = User::where(compact('email'))->first();
+
+        if (empty($user)) {
+            $this->error(sprintf(
+                'No user found for email: %s',
+                $email
+            ));
+            return;
+        }
+
+        $attempt = [
+            'password'              => $this->secret("Enter your new password"),
+            'password_confirmation' => $this->secret('Verify the password'),
+        ];
+
+        if (!$this->validatePassword($attempt)) {
+            $this->error('Invalid password. Run this command again');
+            return;
+        }
+
+        $user->password = bcrypt($attempt['password']);
+        $user->save();
+
+        $this->info('Password updated successfully. Log in at /manage');
+    }
+
+    private function validatePassword(array $attempt) : bool
+    {
+        $validator = Validator::make(
+            $attempt,
+            [
+                'password' => 'required|string|min:6|confirmed',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return false;
+        }
+
+        return true;
     }
 }
